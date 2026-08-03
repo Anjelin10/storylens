09945c58e5d2cf1967c5175c076dd3f03fbddd7e
Anjelin <anjelin1003@gmail.com>
2026-08-03 20:14:44 +0530

    Fetching movie data's and CSS changes

--- js/data.js
+++ js/data.js
@@ -1,11 +1,70 @@
 const BASE_URL = "https://api.themoviedb.org/3";
+
+//List of Popular Movies
 async function getPopularMovies() {
     try{
         const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
+        return data;
     }catch(error){
-        console.log(error);
+        console.error("Error fetching data:", error);
+        return null;
     }
 }
 getPopularMovies()
+
+//List of Top Rated Movies
+async function getTopRatedMovies() {
+    try{
+        const response = await fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`)
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    }catch(error){
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+getTopRatedMovies()
+
+//List of Upcoming Movies
+async function getUpcomingMovies() {
+    try{
+        const response = await fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}`)
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    }catch(error){
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+getUpcomingMovies()
+
+//List of Now Playing Movies
+async function getNowPlayingMovies() {
+    try{
+        const response = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    }catch(error){
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+getNowPlayingMovies()
+
+async function getTrendingMovies() {
+    try{
+        const response = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    }catch(error){
+        console.error("Error fetching data:", error);
+        return null;
+    }
+}
+getTrendingMovies()
--- js/home.js
+++ js/home.js
@@ -1,4 +1,5 @@
 function Home() {
+    // User Avatar and Dropdown
     const userAvatar = document.getElementById("user-avatar");
     const userDropdown = document.getElementById("user-dropdown");
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -19,5 +20,139 @@ function Home() {
             userDropdown.classList.remove("show");
         }
     });
+
+    // Fetching and Displaying Trending Movies
+    getTrendingMovies().then(data => {
+        if (data && data.results) {
+            const trendingMoviesContainer = document.getElementById("trending-movies");
+            data.results.forEach(movie => {
+            trendingMoviesContainer.innerHTML += `
+                <div class="movie-card">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <div class="movie-grid-info">
+                        <div class="movie-grid-title">${movie.title}</div>
+                        <div class="movie-grid-meta">
+                            <span>${movie.vote_average.toFixed(1)}/10</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        }
+    });
+
+    // Fetching and Displaying Popular Movies
+    getPopularMovies().then(data => {
+        if (data && data.results) {
+            const popularMoviesContainer = document.getElementById("popular-movies");
+            data.results.forEach(movie => {
+            popularMoviesContainer.innerHTML += `
+                <div class="movie-card">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <div class="movie-grid-info">
+                        <div class="movie-grid-title">${movie.title}</div>
+                        <div class="movie-grid-meta">
+                            <span>${movie.vote_average.toFixed(1)}/10</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        }
+    });
+
+    // Fetching and Displaying Top Rated Movies
+    getTopRatedMovies().then(data => {
+        if (data && data.results) {
+            const topRatedMoviesContainer = document.getElementById("top-rated-movies");
+            data.results.forEach(movie => {
+            topRatedMoviesContainer.innerHTML += `
+                <div class="movie-card">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <div class="movie-grid-info">
+                        <div class="movie-grid-title">${movie.title}</div>
+                        <div class="movie-grid-meta">
+                            <span>${movie.vote_average.toFixed(1)}/10</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        }
+    });
+
+     // Fetching and Displaying Upcoming Movies
+    getUpcomingMovies().then(data => {
+        if (data && data.results) {
+            const upcomingMoviesContainer = document.getElementById("upcoming-movies");
+            data.results.forEach(movie => {
+            upcomingMoviesContainer.innerHTML += `
+                <div class="movie-card">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <div class="movie-grid-info">
+                        <div class="movie-grid-title">${movie.title}</div>
+                        <div class="movie-grid-meta">
+                            <span>${movie.vote_average.toFixed(1)}/10</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        }
+    });
+
+    // Fetching and Displaying Now Playing Movies
+    getNowPlayingMovies().then(data => {
+        if (data && data.results) {
+            const nowPlayingMoviesContainer = document.getElementById("now-playing-movies");
+            data.results.forEach(movie => {
+            nowPlayingMoviesContainer.innerHTML += `
+                <div class="movie-card">
+                    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+                    <div class="movie-grid-info">
+                        <div class="movie-grid-title">${movie.title}</div>
+                        <div class="movie-grid-meta">
+                            <span>${movie.vote_average.toFixed(1)}/10</span>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        }
+    });
+
+    // Fetching and Displaying Hero Banner Movie
+    getTrendingMovies().then(data => {
+    if(!data || !data.results) return;
+    const hero = document.getElementById("hero");
+    const heroContent = document.getElementById("hero-content");
+    const heroMovies = data.results.slice(0,4);
+    let current = 0;
+    function renderHero(){
+        const movie = heroMovies[current];
+        hero.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`;
+        heroContent.innerHTML = `
+            <h1>${movie.title}</h1>
+            <p class="hero-overview">
+                ${movie.overview.substring(0,150)}...
+            </p>
+            <div class="hero-actions">
+                <button class="btn-primary">
+                    ▶ Watch Trailer
+                </button> 
+            </div>
+        `;
+    }
+    renderHero();
+
+    setInterval(()=>{
+        current++;
+        if(current>=heroMovies.length){
+            current=0;
+        }
+        renderHero();
+        },5000);
+    });
+
 }
 Home();
